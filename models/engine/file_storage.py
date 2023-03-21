99783d289057f416c28136195585a41cd6a319a7
Anto236 <Antonymbugua1237@gmail.com>
2023-03-21 15:10:47 +0000

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ import json
 
 
 class FileStorage:
+
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
@@ -24,12 +25,11 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        ob_dic = {}
+        for k, v in self.__objects.items():
+            ob_dic[k] = v.to_dict()
+        with open(self.__file_path, mode='w', encoding='utf-8') as f:
+            json.dump(ob_dic, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -40,12 +40,11 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
