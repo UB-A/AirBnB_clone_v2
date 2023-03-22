96f5737a58860b7f94c68dfe565315409e9691f3
Anto236 <Antonymbugua1237@gmail.com>
2023-03-22 20:00:16 +0000

    error

--- models/place.py
+++ models/place.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, orm
+from sqlalchemy import DateTime, Column, Integer, String,\
+    Float, Table, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from models.review import Review
 from os import getenv
+from models.review import Review
 
 
 class Place(BaseModel, Base):
+
     """ A place to stay """
     __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        city_id = Column(String(60), ForeignKey('cities.id'),
-                         nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'),
-                         nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        reviews = relationship('Review', backref='place',
-                               cascade='all, delete-orphan')
+        reviews = relationship('Review', backref='place', cascade="delete")
+
     else:
         @property
         def reviews(self):
-            """ Getter attribute to return list of Review instances """
+            """Return list of review instances"""
             from models import storage
             review_list = []
             for review in storage.all(Review).values():
