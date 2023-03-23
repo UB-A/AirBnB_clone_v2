f6b52b367c97fd522f0629eaed7f66365db172ce
Ubong <ubongoben@gmail.com>
2023-03-23 01:08:59 -0700

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class"""
+    '''amenity class'''
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-
-    place_amenities = relationship("Place", secondary="place_amenity")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
