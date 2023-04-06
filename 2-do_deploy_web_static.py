8b4a209ed749f078127aa47cf089588f4717efa9
Anto236 <Antonymbugua1237@gmail.com>
2023-04-06 11:04:21 +0000

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
-from datetime import datetime
+# module with Fabric script that distributes an archive to server
+
 from fabric.api import *
-import shlex
-import os
+import os.path
 
-
-env.hosts = ['100.27.14.9', '3.89.146.79']
-env.user = "ubuntu"
+env.user = 'ubuntu'
+env.hosts = ['35.231.100.106', '35.237.151.115']
 
 
 def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+    """Fabric script that distrubutes an archive to server"""
+    if os.path.exists(archive_path):
+        new_path = archive_path.replace('versions/', '')
+        file_name = new_path[:-4]
+        arc_folder = "/data/web_static/releases/{}".format(new_path)
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(file_name))
+        run('tar -xzf /tmp/{} -C {}'.format(new_path, arc_folder[:-4]))
+        run('rm /tmp/{}'.format(new_path))
+        run('mv {}/web_static/* {}/'.format(arc_folder[:-4], arc_folder[:-4]))
+        run('rm -rf {}/web_static'.format(arc_folder[:-4]))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(arc_folder[:-4]))
         return True
-    except Exception:
+    else:
         return False
